2cfd0e53111e7ce4ff7732a3dcac8415e4bb4c96
Banerjee <kbanerjee@friendsbalt.org>
2021-12-02 11:54:30 -0500

    message

--- LabyrinthSolver.java
+++ LabyrinthSolver.java
@@ -9,27 +9,34 @@ public class LabyrinthSolver {
 	private int right = 2;
 	private int down = 1;
 	private int count = 0;
+	private int [][] moveTracker;
 	
 	public LabyrinthSolver(int r, int c) {
 		this.maze = new Labyrinth(r, c);
 		this.rows = r;
 		this.cols = c;
+		moveTracker = new int[r][c];
 	}
-	public void findSafeMove(int row, int col, Labyrinth maze) {
+	public boolean findSafeMove(int row, int col, Labyrinth maze) {
 		if((row == rows-1) && (col == cols-1)) {
-			return;
-			//show solution
+			for(int i = 0; i < moves.size(); i++) {
+				System.out.println(moves.get(i));
+			}
+			return true;
 			//leave 
 		}
 		//loops through 4 times once for each direction
+		//have you been there before
+		//method encapsulation for the if statements
+		//if next one is a solution return true
+		//remove froms olution array and clear array that keeps track of whre you have been (already been here)
 		
 		while(count < 4){
 			if(count == 0){
 				row += 1;
-				if((maze.isValid(row, col)) && (maze.isStone(row, col))) {
-					moves.add(up);
-					findSafeMove(row, col, maze);
-					goBack(row, col, up);
+				if((maze.isValid(row, col)) && (maze.isStone(row, col))
+					&& (!alrBeenHere(row, col))) {
+					recursiveCall(up);
 				}
 			}
 			if(count == 1) {
@@ -61,6 +68,12 @@ public class LabyrinthSolver {
 		}
 	}
 	
+	public void recursiveCall(int direction) {
+		moves.add(direction);
+		findSafeMove(row, col, maze);
+		goBack(row, col, direction);
+	}
+	
 	public void goBack(int row, int col, int direction) {
 		moves.remove(direction);
 	}
@@ -69,9 +82,16 @@ public class LabyrinthSolver {
 		findSafeMove(0, 0, maze);
 	}
 	
+	public boolean alrBeenHere(int row, int col) {
+		if(moveTracker[row][col])
+			return true;
+		return false;
+	}
+	
 	public static void main(String[] args) {
-		LabyrinthSolver test = new LabyrinthSolver(5, 5);
-		test.solve();
+		//LabyrinthSolver test = new LabyrinthSolver(5, 5);
+		//test.solve();
+		System.out.print("hello");
 	}
 	
 }
