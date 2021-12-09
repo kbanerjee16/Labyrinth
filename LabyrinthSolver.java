0994648b5890eca3a4720c855235535bcae695e8
Banerjee <kbanerjee@friendsbalt.org>
2021-12-09 11:34:58 -0500

    message

--- LabyrinthSolver.java
+++ LabyrinthSolver.java
@@ -1,114 +1,89 @@
 import java.util.ArrayList;
 public class LabyrinthSolver {
-	private Labyrinth maze;
-	private int rows;
-	private int cols;
-	private ArrayList<Integer> moves = new ArrayList<Integer>();
-	private int up = 0;
-	private int left = 3;
-	private int right = 2;
-	private int down = 1;
-	private int count = 0;
-	private boolean [][] moveTracker;
+	//add in line comments, take out main method, check work
+	private static ArrayList<Integer> moves = new ArrayList<Integer>();
+	private static final int up = 0;
+	private static final int left = 2;
+	private static final int right = 3;
+	private static final int down = 1;
+	private static boolean [][] moveTracker;
 	
-	public LabyrinthSolver(int r, int c) {
-		//this.maze = new Labyrinth(r, c);
-		//this.rows = r;
-		//this.cols = c;
-		moveTracker = new boolean [r][c];
-		moveTracker[0][0] = true;
-	}
-	public boolean findSafeMove(int row, int col, Labyrinth maze) {
-		if((row == maze.rows-1) && (col == maze.cols-1)) {
-			printSolution();
+	public static boolean findSafeMove(int row, int col, Labyrinth l) {
+		if((row == l.rows-1) && (col == l.cols-1)) {
 			return true;
 		}
-		count = 0;
-		while(count < 4){
+	
+		for(int count = 0; count < 4; count++){
 			if(count == 0){
-				if(isSafe(row-1, col)) {
+				if(isSafe(row-1, col, l)) {
 					makeMove(row-1, col, up);
-					if(findSafeMove(row-1, col, maze))
+					if(findSafeMove(row-1, col, l))
 						return true;
 					goBack(row-1, col);
 				}
 			}
 			if(count == 1) {
-				if(isSafe(row+1, col)) {
+				if(isSafe(row+1, col, l)) {
 					makeMove(row+1, col, down);
-					if(findSafeMove(row+1, col, maze))
+					if(findSafeMove(row+1, col, l))
 						return true;
 					goBack(row+1, col);
 				}
 			}
 			if(count == 2) {
-				if(isSafe(row, col+1)) {
-					makeMove(row, col+1, right);
-					if(findSafeMove(row, col+1, maze))
-						return true;
-					goBack(row, col+1);
-				}
-			}
-			if(count == 3) {
-				if(isSafe(row, col-1)) {
+				if(isSafe(row, col-1, l)) {
 					makeMove(row, col-1, left);
-					if(findSafeMove(row, col-1, maze))
+					if(findSafeMove(row, col-1, l))
 						return true;
 					goBack(row, col-1);
 				}
 			}
+			if(count == 3) {
+				if(isSafe(row, col+1, l)) {
+					makeMove(row, col+1, right);
+					if(findSafeMove(row, col+1, l))
+						return true;
+					goBack(row, col+1);
+				}
+			}
 				
-			count++;
 		}
 		return false;
 	}
 	
-	public boolean isSafe(int row, int col) {
-		return (maze.isValid(row, col)) && (maze.isStone(row, col)) && (!alrBeenHere(row, col));
+	public static boolean isSafe(int row, int col, Labyrinth l) {
+		return (l.isValid(row, col)) && (l.isStone(row, col)) && (!alrBeenHere(row, col));
 	}
 	
-	public void makeMove(int row, int col, int direction) {
+	public static void makeMove(int row, int col, int direction) {
 		moves.add(direction);
 		moveTracker[row][col] = true;
 	}
 	
-	public void goBack(int row, int col) {
+	public static void goBack(int row, int col) {
 		moveTracker[row][col] = false;
 		moves.remove(moves.size()-1);
 	}
 	
-	public void solve(Labyrinth l) {
+	public static int[] solve(Labyrinth l) {
+		moveTracker = new boolean [l.rows][l.cols];
+		moveTracker[0][0] = true;
 		findSafeMove(0, 0, l);
-		//return moves() don't print it
-	}
-	
-	public void printSolution() {
+		int [] finalMoves = new int [moves.size()];
 		for(int i = 0; i < moves.size(); i++) {
-			if(moves.get(i) == 0)
-				System.out.println("up");
-			if(moves.get(i) == 1)
-				System.out.println("down");
-			if(moves.get(i) == 2)
-				System.out.println("right");
-			if(moves.get(i) == 3)
-				System.out.println("left");
+			finalMoves[i] = moves.get(i);
 		}
+		return finalMoves;
 	}
 	
-	public void printMaze() {
-		maze.printGrid();
-	}
-	
-	public boolean alrBeenHere(int row, int col) {
+	public static boolean alrBeenHere(int row, int col) {
 		return moveTracker[row][col];
 	}
 	
 	public static void main(String[] args) {
-		LabyrinthSolver test = new LabyrinthSolver(7, 7);
-		test.printMaze();
-		test.solve();
+		Labyrinth l = new Labyrinth(4, 4);
+		l.printGrid();
+		System.out.println(l.solves(solve(l)));
 	}
 	
 }
-//make class static no constructor
-//return solution don't print it
